547ffb8d4d62c240d857ed34742a555549e4bdaf
kristen-killingsworth <killingsworth_kristen@yahoo.com>
2023-03-17 00:31:16 -0400

    Google Cloud Run v2

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const cors = require("cors")
 // import sequelize connection
 const sequelize = require(`./config/connection`)
 
+console.log("Loaded FLT")
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
