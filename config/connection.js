247bd0a8e781fb3ec013245989bf2d5514a09322
kristen-killingsworth <killingsworth_kristen@yahoo.com>
2023-03-17 00:14:50 -0400

    google cloud sql

--- config/connection.js
+++ config/connection.js
@@ -3,21 +3,13 @@ require('dotenv').config();
 
 const Sequelize = require('sequelize');
 
-let sequelize;
-
-if (process.env.JAWSDB_URL) {
-  sequelize = new Sequelize(process.env.JAWSDB_URL);
-} else {
-  sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-      host: 'localhost',
-      dialect: 'mysql',
-      port: 3306
-    }
-  );
-}
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+  dialect: 'mysql',
+  host: `/cloudsql/${process.env.SQL_INSTANCE}`,
+  timestamps: false,
+  dialectOptions: {
+    socketPath: `/cloudsql/${process.env.SQL_INSTANCE}`
+},
+});
 
 module.exports = sequelize;
