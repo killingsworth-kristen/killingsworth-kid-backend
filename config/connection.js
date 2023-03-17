3106edfd9072980f61d36552f4309b02118eac84
kristen-killingsworth <killingsworth_kristen@yahoo.com>
2023-03-17 00:41:07 -0400

    Google Cloud v3

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,7 @@
 require('dotenv').config();
 
 const Sequelize = require('sequelize');
-
+console.log("Pre-Connection")
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
   dialect: 'mysql',
   host: `/cloudsql/${process.env.SQL_INSTANCE}`,
@@ -11,5 +11,7 @@ const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, proces
     socketPath: `/cloudsql/${process.env.SQL_INSTANCE}`
 },
 });
+
+console.log("Post-connection")
 
 module.exports = sequelize;
